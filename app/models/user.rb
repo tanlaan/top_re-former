42a817100bfab595114d742ef7242de0ba3bf6cb
Chris Bolas <tanlaan33@gmail.com>
2021-12-07 15:05:08 -0800

    Add minimum to password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true
+  validates_length_of :password, minimum: 6
 end
