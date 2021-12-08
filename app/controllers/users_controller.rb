3ad4d143db678c3345a45857080536093772c8fe
Chris Bolas <tanlaan33@gmail.com>
2021-12-07 19:44:00 -0800

    Add edit/update funcionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_users_path
+      redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :edit
     end
   end
 
